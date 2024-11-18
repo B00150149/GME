5a035b81209b743b3763bf75e5cec459c032ef2b
sibeil Rock <b00156196@mytudublin.ie>
2024-11-18 07:34:01 +0000

    style sibeil

--- my-app/src/app/about-us/page.js
+++ my-app/src/app/about-us/page.js
@@ -1,11 +1,7 @@
 import * as React from 'react';
-import Header from '../components/Header';  // Update path if needed
-import Footer from '../components/Footer';  // Update path if needed
-
-// Add Bootstrap
+import Header from '../components/Header'; 
+import Footer from '../components/Footer';  
 import 'bootstrap/dist/css/bootstrap.min.css';
-
-// Import the CSS file
 import '../styles/Login.css';
 import '../styles/Style.css';
 
