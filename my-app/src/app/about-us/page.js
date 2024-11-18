0cf71d7b91720e3e5037c94ade1b905c77c59d14
sibeil Rock <b00156196@mytudublin.ie>
2024-11-18 08:15:44 +0000

    contact form

--- my-app/src/app/about-us/page.js
+++ my-app/src/app/about-us/page.js
@@ -1,6 +1,6 @@
 import * as React from 'react';
 import Header from '../components/Header'; 
-import Footer from '../components/Footer';  
+import Footer from '../components/Footer'; 
 import 'bootstrap/dist/css/bootstrap.min.css';
 import '../styles/Login.css';
 import '../styles/Style.css';
@@ -13,6 +13,40 @@ export default function About() {
           <Header />
           <h1>About</h1>
           <h2>Mission statement here*</h2>
+          <h1>Contact Us</h1>
+        
+          
+
+
+<div class="container contact-form">
+            <div class="contact-image">
+                <img src="https://image.ibb.co/kUagtU/rocket_contact.png" alt="rocket_contact"/>
+            </div>
+            <form method="post">
+                <h3>Drop Us a Message</h3>
+               <div class="row">
+                    <div class="col-md-6">
+                        <div class="form-group">
+                            <input type="text" name="txtName" class="form-control" placeholder="Your Name *" value="" />
+                        </div>
+                        <div class="form-group">
+                            <input type="text" name="txtEmail" class="form-control" placeholder="Your Email *" value="" />
+                        </div>
+                        <div class="form-group">
+                            <input type="text" name="txtPhone" class="form-control" placeholder="Your Phone Number *" value="" />
+                        </div>
+                        <div class="form-group">
+                            <input type="submit" name="btnSubmit" class="btnContact" value="Send Message" />
+                        </div>
+                    </div>
+                    <div class="col-md-6">
+                        <div class="form-group">
+                            <textarea name="txtMsg" class="form-control" placeholder="Your Message *" style="width: 100%; height: 150px;"></textarea>
+                        </div>
+                    </div>
+                </div>
+            </form>
+</div>
           <Footer/>
          </>
     );
